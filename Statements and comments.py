31a2bfd4611af9387731c6e8a702767715e24834
Mohammed_Haider <mhfadoo@gmail.com>
2025-02-15 17:27:08 +0500

    States and comms

---
+++ Statements and comments.py
@@ -0,0 +1,13 @@
+#Comments in Python
+
+print("My name is Haider, I live in Pakistan. ")
+
+print("I love to code as it is my passion.")
+
+print("The language in programming which I love is Python.")
+
+print("A more farvioute hobby of mine is Football.")
+
+print("My favriote player in football is CR7 as he has a good mindset.")
+
+print(7)
