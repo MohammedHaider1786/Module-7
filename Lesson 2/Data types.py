ca673ff1d815a7a4081d89e0712c4c4683993e72
Mohammed_Haider <mhfadoo@gmail.com>
2025-02-16 18:00:07 +0500

    string operations

--- Lesson 2/Data types.py
+++ Lesson 2/Data types.py
@@ -8,7 +8,14 @@ print("DT of the name is", type(Age))
 print("DT of the name is", type(Weight))
 print("DT of the name is", type(isstudent))
 
+#type casting
+
 weight = str(Weight)
 Age = int(Age)
 Name = str(Name)
 isstudent = int(isstudent)
+
+print("DT after type casting is ", type(weight))
+print("DT after type casting is ", type(Age))
+print("DT after type casting is ", type(Name))
+print("DT after type casting is ", type(isstudent))
---
+++ Lesson 2/String operations.py
@@ -0,0 +1,16 @@
+firstname = input("PLease enter your first name: ")
+lastname = input("Please enter your last name: ")
+fullname = firstname+" "+lastname
+
+print("Hi there ",fullname)
+
+firstname2 = "Mohammed"
+lastname2 = "Haider"
+print("firstname is ", firstname2)
+print("lastname is ", lastname2)
+print("fullname is ", fullname)
+print("firstname*4 is ", firstname2*4 )
+
+print("the characters of the full name are ", len(fullname))
+print("the second character of the name is ", fullname[1])
+print("Sliced name is ", fullname[1:8])
