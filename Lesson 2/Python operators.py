377cab3018b796b7356ae7481f7eeb56ccb8e3ec
Mohammed_Haider <mhfadoo@gmail.com>
2025-02-16 17:40:25 +0500

    Python Operators

---
+++ Lesson 2/Python operators.py
@@ -0,0 +1,14 @@
+Num1 = 300
+Num2 = 13
+
+print("The sum of " ,Num1, " and " ,Num2," is equalls to ",Num1+Num2)
+print("The differnce of " ,Num1, " and " ,Num2," is equalls to ",Num1-Num2)
+print("The product of " ,Num1, " and " ,Num2," is equalls to ",Num1*Num2)
+print("The division of " ,Num1, " and " ,Num2," is equalls to ",Num1/Num2)
+print("The floor division of " ,Num1, " and " ,Num2," is equalls to ",Num1//Num2)
+print("The Module of " ,Num1, " and " ,Num2," is equalls to ",Num1%Num2)
+print(Num1," to the power of ", Num2,"is ",Num1**Num2,)
+
+print("is" ,Num1,"equal to ",Num2, Num1==Num2 )
+print(Num1, " is greater than ",Num2 ,Num1>Num2 )
+print(Num2, " is greater than ",Num1 ,Num2>Num1 )
