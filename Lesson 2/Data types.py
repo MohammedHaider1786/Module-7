8039eba559f343daaadb6804daf437019c64e3ee
Mohammed_Haider <mhfadoo@gmail.com>
2025-02-16 17:18:15 +0500

    Data types

---
+++ Lesson 2/Data types.py
@@ -0,0 +1,14 @@
+Name = "Haider"
+Age = 14
+Weight = 56.5
+isstudent = True
+
+print("DT of the name is", type(Name))
+print("DT of the name is", type(Age))
+print("DT of the name is", type(Weight))
+print("DT of the name is", type(isstudent))
+
+weight = str(Weight)
+Age = int(Age)
+Name = str(Name)
+isstudent = int(isstudent)
