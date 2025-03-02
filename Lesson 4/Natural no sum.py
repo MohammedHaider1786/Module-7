65423ae70495fe0ec59602d4365c6e2f9c78cf84
Mohammed_Haider <mhfadoo@gmail.com>
2025-03-02 17:36:36 +0500

    Natural no sum

---
+++ Lesson 4/Natural no sum.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the range of the numbers: "))
+print("The range of numbers is", n)
+sum1=0
+for i in range(1, n+1):
+    sum1+=i
+print("Sum of 10 natural numbers is ",sum1)
+
+totalsum=sum(range(1,11))
+print("Total sum of 10 natural numbers is ",totalsum)
+
+
